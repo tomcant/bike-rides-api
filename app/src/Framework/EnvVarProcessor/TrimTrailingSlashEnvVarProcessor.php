035b3ed31678c335f18f3a152123671dc1ba4edc
Tom Cant <tomcant@gmail.com>
2024-05-27 15:32:17 +0100

    Trim trailing slash on API URL env var

---
+++ app/src/Framework/EnvVarProcessor/TrimTrailingSlashEnvVarProcessor.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace App\Framework\EnvVarProcessor;
+
+use Symfony\Component\DependencyInjection\EnvVarProcessorInterface;
+
+final readonly class TrimTrailingSlashEnvVarProcessor implements EnvVarProcessorInterface
+{
+    public function getEnv(string $prefix, string $name, \Closure $getEnv): string
+    {
+        return \trim($getEnv($name), '/');
+    }
+
+    public static function getProvidedTypes(): array
+    {
+        return [
+            'trim-trailing-slash' => 'string',
+        ];
+    }
+}
